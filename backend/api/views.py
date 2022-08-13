fd70f1c3d996076433d321650c2c250dd07eef9a
ayeshoali <ayeshoali@gmail.com>
2022-08-14 01:53:47 +0500

    echoing GET or POST request data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,21 @@
+import json
 from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
-  return JsonResponse({'message': "Hi there your JSON response"}
+
+  body = request.body 
+  print(body)
+  # print(request.GET)
+  # print(dict(request.headers))
+  data = {}
+  try:
+    data = json.loads(body)
+  except:
+    pass
+  # print(data)
+  data['param'] = dict(request.GET)
+  data['headers'] = dict(request.headers)
+  data['content_type'] = request.content_type
+  return JsonResponse(data
   )
