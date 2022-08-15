22a05a388f6ac0cbb072f3390225fb01f9b521e8
ayeshoali <ayeshoali@gmail.com>
2022-08-15 18:03:11 +0500

    mixing and genericView

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -1,9 +1,36 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 
 
 from .models import Product
 from .serializers import ProductSerializer
 
+
+class ProductMixinView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView
+    ):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def get(self, request, *args, **kwargs): #HTTP -> get
+        pk = kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = "this is a single view doing cool stuff"
+        serializer.save(content=content)
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
@@ -37,7 +64,7 @@ class ProductDeleteAPIView(generics.DestroyAPIView):
     def perform_update(self, serializer):
         instance = serializer.save()
 
-        
+
 # class ProductListAPIView(generics.ListAPIView):
 #     queryset = Product.objects.all()
 #     serializer_class = ProductSerializer
