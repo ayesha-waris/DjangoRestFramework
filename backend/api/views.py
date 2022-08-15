83da90f17651f11353aea09d857bd41fb6bc5639
ayeshoali <ayeshoali@gmail.com>
2022-08-15 14:03:07 +0500

    convertung view to APIView with decorator

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,19 @@
 import json
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
+
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from projects.models import Product
 
+@api_view(['GET', 'POST'])
 def api_home(request, *args, **kwargs):
 
   model_data = Product.objects.get(pk=1)
+  print(model_data)
   data = {}
   if(model_data):
-    data['id'] = model_data.id
-    data['title'] = model_data.title
-    data['content'] = model_data.content
-    data['price'] = model_data.price
-  return JsonResponse(data
-  )
+    data = model_to_dict(model_data)
+  return Response(data)
