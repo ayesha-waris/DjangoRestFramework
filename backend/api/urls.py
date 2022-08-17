d579013842d8eb6096d552fc3f23689575a01bb6
ayeshoali <ayeshoali@gmail.com>
2022-08-17 23:50:44 +0500

    chore: add token authentication for list and create APIView

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,12 @@
 from django import views
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 from . import views
 
 
 urlpatterns = [
+    path('auth/', obtain_auth_token),
     path('', views.api_home.as_view()),
 
 ]
--- backend/projects/views.py
+++ backend/projects/views.py
@@ -1,9 +1,24 @@
 from rest_framework import authentication, generics, mixins, permissions
 
-
+from api.authentication import TokenAuthentication
 from .models import Product
 from .permissions import IsStaffEditorPermission
 from .serializers import ProductSerializer
+
+
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication,
+                              TokenAuthentication
+                              ]
+    permission_classes = [permissions.IsAdminUser,
+                          IsStaffEditorPermission]  # ordering matters
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data
+        print(serializer.validated_data)
+        serializer.save()
 
 
 class ProductMixinView(
@@ -39,16 +54,7 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     serializer_class = ProductSerializer
 
 
-class ProductListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
 
-    def perform_create(self, serializer):
-        title = serializer.validated_data
-        print(serializer.validated_data)
-        serializer.save()
 
 
 class ProductUpdateAPIView(generics.UpdateAPIView):
