6bdb4dd8aea6546ae68c1b1c9dfa714aade0066e
ayeshoali <ayeshoali@gmail.com>
2022-08-19 01:38:25 +0500

    chore: add url using hyperlinkedIdentityField

--- backend/projects/serializers.py
+++ backend/projects/serializers.py
@@ -1,15 +1,18 @@
+from requests import request
 from rest_framework import serializers
-from rest_framework import reverse
+from rest_framework.reverse  import reverse
 from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
-    url = serializers.SerializerMethodField(read_only = True)
+    edit_url = serializers.SerializerMethodField(read_only = True)
+    url = serializers.HyperlinkedIdentityField(view_name="product-detail", lookup_field='pk')
     class Meta:
         model = Product
         fields = [
             'url',
+            'edit_url',
             'pk',
             'title',
             'content',
@@ -18,8 +21,12 @@ class ProductSerializer(serializers.ModelSerializer):
             'my_discount',
         ]
 
-    def get_url(self, obj):
-      return f'/api/v2/products/{obj.pk}/'
+    def get_edit_url(self, obj):
+      # return f'/api/v2/products/{obj.pk}/'
+      request = self.context.get('request')
+      if reverse is None:
+        return None
+      return reverse('product-detail', kwargs={'pk': obj.pk}, request=request)
 
     def get_my_discount(self, obj):
         if not isinstance(obj, Product):
